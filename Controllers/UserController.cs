fc0cdd2d8e90c33c582d477b719ce9fef0254823
2Sravs <sravani.chekuri1243#gmail.com>
2023-10-10 23:41:16 +0530

    firstcommit

---
+++ Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using Bank.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bank.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly Users user = new Users()
+        {
+            Id = 1,
+            UserName = "Sravs",
+            Email = "sravs@gmail.com",
+            Password = "sravs@123"
+        };
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(Users model)
+        {
+            if (ModelState.IsValid && model.UserName == user.UserName && model.Email == user.Email && model.Password == user.Password)
+            {
+                return RedirectToAction("Index", "Dashboard");
+            }
+            ModelState.AddModelError("", "Invalid username or Password");
+            return View(model);
+        }
+    }
+}
